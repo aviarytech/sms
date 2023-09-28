aaf75a91af770b8ae33f0cd5730e0a7e0eb35525
Brian Richter <brianorwhatever@gmail.com>
2023-09-27 23:30:01 -0700

    log

--- index.ts
+++ index.ts
@@ -3,22 +3,28 @@ const server = Bun.serve({
   port: 7695,
   async fetch(req: Request) {
     const url = new URL(req.url);
-    if (url.pathname === '/sms-webhook') {
-      const formData = await req.formData();
-      const messageSid = formData.get('MessageSid');
-      const from = formData.get('From');
-      const body = formData.get('Body');
+    try {
+      if (url.pathname === '/sms-webhook') {
+        const formData = await req.formData();
+        const messageSid = formData.get('MessageSid');
+        const from = formData.get('From');
+        const body = formData.get('Body');
 
-      console.log(`Received SMS [${messageSid}] from ${from}: ${body}`);
-      
-      const twimlResponse = `
-        <Response>
-          <Message>Thank you for your message.</Message>
-        </Response>
-      `;
-      return new Response(twimlResponse, { headers: { 'Content-Type': 'text/xml' } });
+        console.log(`Received SMS [${messageSid}] \n
+        from ${from}: ${body}`);
+        
+        const twimlResponse = `
+          <Response>
+            <Message>Thank you for your message.</Message>
+          </Response>
+        `;
+        return new Response(twimlResponse, { headers: { 'Content-Type': 'text/xml' } });
+      }
+      return new Response('Not Found', { status: 404 });
+    } catch(e) {
+      console.error(e);
+      return new Response('Bad Bad Request', { status: 400 });
     }
-    return new Response('Not Found', { status: 404 });
   },
 });
 
