919673381df0bdab5c68551c2a92b9b05bf7cd66
Brian Richter <brianorwhatever@gmail.com>
2023-09-27 23:14:27 -0700

    769759

--- index.ts
+++ index.ts
@@ -1,9 +1,23 @@
-const server = Bun.serve({
-  hostname: "::",
-  port: process.env.PORT ?? 3000,
-  fetch(request) {
-    return new Response("Welcome to Bun!");
+
+Bun.serve({
+  port: 769759,
+  async fetch(req: Request) {
+    const url = new URL(req.url);
+    if (url.pathname === '/sms-webhook') {
+      const formData = await req.formData();
+      const messageSid = formData.get('MessageSid');
+      const from = formData.get('From');
+      const body = formData.get('Body');
+
+      console.log(`Received SMS [${messageSid}] from ${from}: ${body}`);
+      
+      const twimlResponse = `
+        <Response>
+          <Message>Thank you for your message.</Message>
+        </Response>
+      `;
+      return new Response(twimlResponse, { headers: { 'Content-Type': 'text/xml' } });
+    }
+    return new Response('Not Found', { status: 404 });
   },
 });
-
-console.log(`Listening on http://localhost:${server.port}`);
